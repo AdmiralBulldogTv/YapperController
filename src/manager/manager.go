bf9af821571471525f2636354f8382a8528f5da0
denning <denning.campbell@gmail.com>
2022-02-23 22:35:22 -0800

    new voices

--- src/manager/manager.go
+++ src/manager/manager.go
@@ -91,6 +91,8 @@ func (m *Manager) handleSe(gCtx global.Context) error {
 					textparser.VoicesMap["narr1"],
 					textparser.VoicesMap["narr2"],
 					textparser.VoicesMap["narr3"],
+					textparser.VoicesMap["kratos"],
+					textparser.VoicesMap["lamar"],
 				}
 
 				var evnt string
